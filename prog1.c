e38ff069f24b80cec4bd5465ab9d33b4c9a526eb
João André Pestre <joaoandre.pestre@poli.ufrj.br>
2018-11-18 20:42:20 -0200

    Adiciona nova msg

--- prog1.c
+++ prog1.c
@@ -9,106 +9,128 @@ int main(void)
     int fork_ret;
     int pipe1[2];
     int pipe2[2];
-    int pipe3[2];
 
     pipe(pipe1);
     pipe(pipe2);
-    pipe(pipe3);
     
-    // ********* Insira um comando para pegar o PID do processo corrente e mostre na
+    //  Insira um comando para pegar o PID do processo corrente e mostre na
     //  tela da console.
     id = getpid();
     printf("PID do processo corrente: %d\n\n", id);
 
-    // if (*** insira um comando para criar um subprocesso) {
+    // if (insira um comando para criar um subprocesso) {
     if ( (fork_ret = fork()) > 0) {
-        // ***** Faça com que o processo pai execute este trecho de código
+        // Faça com que o processo pai execute este trecho de código
         // Feito pelo condicional -> fork retorna 0 para processo filho e o id do filho ( > 0) para processo pai
 
         printf("Processo pai sendo executado\n\n");
 
-        // ***** Mostre na console o PID do processo pai e do processo filho
+        // Mostre na console o PID do processo pai e do processo filho
+        // Como este é o processo pai, o id do processo corrente é o seu id
+        // E o id do filho é o retorno do fork
         printf("PAI\tPID atual: %d\n     \tPID filho: %d\n\n",id,fork_ret);
 
-        // ***** Monte uma mensagem e a envie para o processo filho
-        // Pipe 1 representa uma mesagem do pai para o filho
-        // Logo, o pai fecha o input 
-        char msg_envia[30] = "Olá filho, tudo bem?";
+        // Monte uma mensagem e a envie para o processo filho
+        char msg_envia[21] = "Olá filho, tudo bem?";
+
+        // Pipe 1 representa mesagens do pai para o filho
+        // O pai fecha a ponta de leitura do pipe 1
         close(pipe1[0]);
-        write(pipe1[1],msg_envia,30);
+        // Escreve na ponta de escrita
+        write(pipe1[1],msg_envia,21);
+        // E fecha a ponta de escrita
         close(pipe1[1]);
 
-        // ***** Mostre na tela o texto da mensagem enviada
+        // Mostre na tela o texto da mensagem enviada
         printf("PAI\tMensagem enviada: %s\n\n", msg_envia);
 
-        // ***** Aguarde a resposta do processo filho
-        //wait(NULL); -- Inserir esse wait faria a espera ser para o processo inteiro, é isso que queremos mesmo?
-
-        // ***** Mostre na tela o texto recebido do processo filho
-        char msg_recebe[30];
+        // Aguarde a resposta do processo filho
+        char msg_recebe[23];
+        // Pipe 2 representa mensagens do filho para o pai
+        // O pai fecha a ponta de escrita do pipe 2
         close(pipe2[1]);
-        read(pipe2[0],msg_recebe,30);
-        close(pipe2[0]);
+        // Lê da ponta de leitura
+        read(pipe2[0],msg_recebe,23);
+
+        // Mostre na tela o texto recebido do processo filho
         printf("PAI\tResposta recebida: %s\n\n", msg_recebe);
 
-
-
-        // ***** Aguarde mensagem do filho e mostre o texto recebido
-        char msg_recebe2[12];
-        close(pipe3[1]);
-        read(pipe3[0],msg_recebe2,12);
+        // Aguarde mensagem do filho
+        // Para isso a ponta de leitura do pipe 2 foi mantida aberta
+        // O pai lê da ponta de leitura 
+        read(pipe2[0],msg_recebe,12);
+        // E fecha a ponta de leitura
         close(pipe2[0]);
-        printf("PAI\tResposta recebida: %s\n\n", msg_recebe2);
+
+        // e mostre o texto recebido
+        printf("PAI\tMensagem recebida: %s\n\n", msg_recebe);
       
-          // ***** Aguarde o término do processo filho
+        // Aguarde o término do processo filho
         wait(&status);
-        // ***** Informe na tela que o filho terminou e que o processo pai também vai encerrarar
+        // Informe na tela que o filho terminou e que o processo pai também vai encerrarar
         printf("PAI\tProcesso filho terminado, processo pai também ira terminar.\n\n");
 
     } else {
-        printf("Processo filho sendo executado\n\n");
-
-        // ***** Faça com que o processo filho execute este trecho de código
+        // Faça com que o processo filho execute este trecho de código
         // Definido pelo condicional -> processos filhos tem fork() retornando zero
-        
-        //  ***** Mostre na tela o PID do processo corrente e do processo pai
+
+        printf("Processo filho sendo executado\n\n");
+    
+        // Como este é o processo filho, é necessário obter o seu id por getpid
+        // E o id do seu pai por getppid
+        id = getpid();
         int ppid = getppid();
+
+        // Mostre na tela o PID do processo corrente e do processo pai
         printf("FILHO\tPID atual: %d\n       \tPID pai: %d\n\n",id,ppid);
 
-        // ***** Aguarde a mensagem do processo pai e ao receber mostre o texto na tela
-        char msg_recebe[30];
+        // Aguarde a mensagem do processo pai 
+        char msg_recebe[21];
+        // Pipe 1 representa mensagens do pai para o filho
+        // O filho fecha a ponta de escrita do pipe 1
         close(pipe1[1]);
-        read(pipe1[0], msg_recebe,30);
+        // Lê a ponta de leitura
+        read(pipe1[0], msg_recebe,21);
+        // E fecha a ponta de leitura
         close(pipe1[0]);
+
+        // Mostre o texto na tela
         printf("FILHO\tMensagem recebida: %s\n\n",msg_recebe);
 
-        // ***** Envie uma mensagem resposta ao pai
-        char msg_envia[30] = "Olá pai, tudo bem sim!";
+        // Envie uma mensagem resposta ao pai
+        char msg_envia[23] = "Olá pai, tudo bem sim!";
+        // Pipe 2 representa mensagens do filho para o pai
+        // O filho fecha a ponta de leitura
         close(pipe2[0]);
-        write(pipe2[1],msg_envia,30);
-        close(pipe2[1]);
+        // Escreve na ponta de escrita
+        write(pipe2[1],msg_envia,23);
+
         printf("FILHO\tResposta enviada: %s\n\n", msg_envia);
         
-        // ***** Execute o comando “for” abaixo
+        // Execute o comando “for” abaixo
         for (j = 0; j <= 10000; j++);
-        char msg[12];
-        // ***** Envie mensagem ao processo pai com o valor final de “j”
-        sprintf(msg,"%d",j);
-        close(pipe3[0]);
-        write(pipe3[1],msg,12);
-        printf("FILHO\tMensagem enviada: %s\n\n", msg);
-        close(pipe3[1]);
 
-        // // ***** Execute o comando abaixo e responda às perguntas
+        // Envie mensagem ao processo pai com o valor final de “j”
+        sprintf(msg_envia,"%d",j);
+        // A ponta de escrita do pipe 2 foi mantida aberta
+        // Porque o filho escreve novamente na ponta de escrita
+        write(pipe2[1],msg_envia,12);
+        // E fecha a ponta de escrita
+        close(pipe2[1]);
+        
+        printf("FILHO\tMensagem enviada: %s\n\n", msg_envia);
+        
+
+        // Execute o comando abaixo e responda às perguntas
         execl("/Bin/ls", "ls", NULL);
         
-        // // ***** O que acontece após este comando?
+        // O que acontece após este comando?
         // O programa tenta executar o programa ls. Caso ele consiga, o processo atual é substituido pelo do ls.
-        // // ***** O que pode acontecer se o comando “execl” falhar?
+        // O que pode acontecer se o comando “execl” falhar?
         // Caso falhe, o programa segue normalmente, executando as mensagens abaixo
         
         printf("FILHO\tSe execl não falhar, essa mensagem não aparecerá no console.\n\n");
-        printf("FILHO\tComo 'bin' está escrito incorretamente, execl falhará e essa mensagem aparecerá no concole.\n\n");
+        printf("FILHO\tComo 'bin' está escrito incorretamente, execl falhará e essa mensagem aparecerá no console.\n\n");
     }
     exit(0);
 }
